b49165b6860a6fbc6f12582e3b649a8c20b4c92e
Prince Anand <120587634+Prince7279@users.noreply.github.com>
2026-06-29 19:37:46 +0530

    kubernetes manifest added

--- app/app.js
+++ app/app.js
@@ -8,9 +8,7 @@ app.get("/", (req, res) => {
 });
 
 app.get("/health", (req, res) => {
-  res.status(200).json({
-    status: "UP",
-  });
+  res.status(200).send("OK");
 });
 
 app.listen(PORT, () => {
